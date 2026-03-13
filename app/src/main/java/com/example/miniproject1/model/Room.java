e6c11a4e252303a9058a24438660344938028fa9
8thMay03 <123087852+8thMay03@users.noreply.github.com>
2026-03-13 10:30:49 +0700

    init UI

---
+++ app/src/main/java/com/example/miniproject1/model/Room.java
@@ -0,0 +1,70 @@
+package com.example.miniproject1.model;
+
+public class Room {
+    private String roomId;
+    private String roomName;
+    private double price;
+    private String status;
+    private String tenantName;
+    private String phone;
+
+    public Room() {
+    }
+
+    public Room(String roomId, String roomName, double price, String status, String tenantName, String phone) {
+        this.roomId = roomId;
+        this.roomName = roomName;
+        this.price = price;
+        this.status = status;
+        this.tenantName = tenantName;
+        this.phone = phone;
+    }
+
+    public String getRoomId() {
+        return roomId;
+    }
+
+    public void setRoomId(String roomId) {
+        this.roomId = roomId;
+    }
+
+    public String getRoomName() {
+        return roomName;
+    }
+
+    public void setRoomName(String roomName) {
+        this.roomName = roomName;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getTenantName() {
+        return tenantName;
+    }
+
+    public void setTenantName(String tenantName) {
+        this.tenantName = tenantName;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+}
